68dd9ac4ac300495fee021826fc8c6e8efc0402f
Wilson S. Martins <wsantmartins@gmail.com>
2023-11-09 10:20:53 +0100

    09/11/2023

---
+++ codes/python/periodic_detuning/oprts.py
@@ -0,0 +1,57 @@
+#script for the operator's definition
+import qutip as qt 
+from stt_qtt import*
+
+#defining the single-body operators
+sx = qt.sigmax()
+sy = qt.sigmay()
+sz = qt.sigmaz()
+sp = qt.sigmap()
+sm = qt.sigmam()
+sn = sp*sm
+
+# Setup operators for individual qubits
+sx_list, sy_list, sz_list, sp_list, sm_list, sn_list = [], [], [], [], [], []
+
+for i in range(N):
+    op_list = [qt.qeye(2)]*N
+    op_list[i] = sx
+    sx_list.append(qt.tensor(op_list))
+    op_list[i] = sy
+    sy_list.append(qt.tensor(op_list))
+    op_list[i] = sz
+    sz_list.append(qt.tensor(op_list))
+    op_list[i] = sp
+    sp_list.append(qt.tensor(op_list))
+    op_list[i] = sm
+    sm_list.append(qt.tensor(op_list))
+    op_list[i] = sn
+    sn_list.append(qt.tensor(op_list))
+
+#magnetization terms 
+H0 = 0
+mag = 0
+magx = 0
+magy = 0
+magz = 0
+magp = 0
+magm = 0
+for i in range(N):
+    mag += sn_list[i]
+    magx += sx_list[i]
+    magy += sy_list[i]
+    magz += sz_list[i]
+    magp += sp_list[i]
+    magm += sm_list[i]
+
+#interaction terms: here snn is the two-body interaction
+snn = 0
+if N == 1:
+    snn = 0.0
+else:
+    for i in range(N - 1):
+        snn += sn_list[i]*sn_list[i+1]
+
+#time-independent part of the Hamiltonian for time dependent detuning
+H0 = bigomega*magx + V*snn
+Hv = mag 
---
+++ codes/python/periodic_rabi/floquet_magnus.py
@@ -0,0 +1,47 @@
+#This script compute the Floquet Magnus expansion at order n
+from stt_qtt import*
+from oprts import*
+import qutip as qt 
+import numpy as np
+import math
+import time
+import matplotlib.pyplot as plt
+
+#Hamiltonians for each half-period for omega variable
+Hp = bigomega*Hv + H0
+Hm = H0
+
+#Hamiltonian for each half period are defined as H1 and H2
+X = -0.5j*Hp
+Y = -0.5j*Hm
+
+#first term of the expansion
+P1 = tau*(X + Y)
+    
+#P is the only term that has to be calculated explicitly in this program
+def P(i, X, Y, tau):
+    P_aux = 0.0
+    for k in range(0, i + 1):
+        P_aux += ((X**(i-k))*(Y**k))/(math.factorial(i-k)*math.factorial(k))
+    return (tau**i)*P_aux
+
+def Q(i, j):
+    if j <= 2 or j >= i:
+        return 0.0
+    else:
+        Qsum = 0.0
+        for k in range(1, i - j + 2):
+            Qsum += Q(k, 1)*Q(i - j,k - 1)
+    return Qsum
+
+def R(i):
+    if i == 1:
+        return P1 
+    else:
+        Qtot = 0.0
+        for k in range(2, i + 1):
+            Qtot += - Q(i,k)/math.factorial(k)      
+        return P(i, X, Y, tau) - Qtot 
+
+    
+# plot
